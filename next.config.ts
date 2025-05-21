8ae2d922240220cde580b81398ed066165ea558a
termozas <alexeevpavel7@gmail.com>
2025-05-22 00:05:56 +0200

    Fix Turbopack WASM bindings error

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  experimental: {
+    turbo: false
+  }
 };
 
 export default nextConfig;
